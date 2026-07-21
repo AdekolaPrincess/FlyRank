1c8e8ebdf46922c4ef1fcf28658d733e06748b75
Adekola Princess <adekolaprincess02@gmail.com>
2026-07-21 17:22:06 +0100

    Stage 1: root and health endpoints

--- BE-02/CRUD API/main.py
+++ BE-02/CRUD API/main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
 app = FastAPI()
+
 @app.get("/")
 def read_root():
-    return {"message": "Hello from my Task API!"}
+    return {"name": "Task API",
+            "version": "1.0",
+            "endpoints": ["/tasks"]
+             }
 
+@app.get("/health")
+def health_check():
+    return {"status": "ok"}
